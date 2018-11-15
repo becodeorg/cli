c46bc2ec4d2cc818692f25369daa8cc14a870238
leny <info@flatland.be>
2018-11-15 14:56:42 +0100

    Add post-install script

---
+++ src/post-install.js
@@ -0,0 +1,28 @@
+/* becodeorg/cli
+ *
+ * /src/post-install.js - Post-install message
+ *
+ * coded by leny@BeCode
+ * started at 15/11/2018
+ */
+
+import chalk from "chalk";
+
+const message = [
+    "🎉",
+    chalk.green("Yeah!"),
+    chalk.cyan("BeCode CLI"),
+    "is installed!",
+    "\n",
+    "\n",
+    "You can now run",
+    chalk.yellow("becode configure"),
+    "to setup your preferences.",
+    "\n",
+    "You can also run",
+    chalk.yellow("becode --help"),
+    "for more information.",
+    "\n",
+];
+
+console.log(...message); // eslint-disable-line no-console
